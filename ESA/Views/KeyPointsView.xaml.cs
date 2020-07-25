bbce9ff83c46ed7537880e5b5bcb3e65789a2658
douglasTafeSA <douglas.hudson-walker@student.tafesa.edu.au>
2020-07-25 11:20:46 +0930

    KeyPoints Diagrams HotFix

--- ESA/Views/KeyPointsView.xaml.cs
+++ ESA/Views/KeyPointsView.xaml.cs
@@ -19,7 +19,7 @@ namespace ESA.Views
     public partial class KeyPointsView : ContentView
     {
         public DetailsViewModel procedureViewModel;
-        public int currentStep;
+        public int currentPointNumber;
 
         public KeyPointsView() { InitializeComponent(); }
 
@@ -27,40 +27,50 @@ namespace ESA.Views
         {
             InitializeComponent();
             procedureViewModel = pvm;
+            
+            foreach(KeyPoint kp in procedureViewModel.Procedure.KeyPoints)
+            {
+                kp.HasDiagram = kp.DiagramURL != "" || kp.DiagramURL != null;
+            }
+
             BindingContext = procedureViewModel.Procedure;
-            currentStep = 1;
+
+            
+            currentPointNumber = 1;
         }
 
         private void KeyPointsContent_TapEventHandler(object sender, EventArgs e)
         {
             // get the current step
-            KeyPoint prevStep = procedureViewModel.Procedure.KeyPoints.ElementAt(this.currentStep);
+            //KeyPoint prevStep = procedureViewModel.Procedure.KeyPoints.ElementAt(this.currentPointNumber);
             // get the step clicked
-            KeyPoint currStep = (KeyPoint)((StackLayout)sender).BindingContext;
-            this.currentStep = currStep.Number;
+            StackLayout currPointParentLayout = ((StackLayout)sender);
+            KeyPoint currPoint = (KeyPoint)currPointParentLayout.BindingContext;
+            StackLayout currPointGrandparent = (StackLayout)currPointParentLayout.Parent;
+            this.currentPointNumber = currPoint.Number;
 
             // get the current point layout
-            StackLayout currPointLayout = ((StackLayout)pointsLayout.Children.ElementAt(currStep.Number));
-            StackLayout currentPointContentLayout = ((StackLayout)currPointLayout.Children.First());
+            //StackLayout currPointLayout = ((StackLayout)pointsLayout.Children.ElementAt(currStep.Number));
+            //StackLayout currentPointContentLayout = ((StackLayout)currPointLayout.Children.First());
 
             // Diagram drop down
-            if (currStep.HasDiagram)
+            if (currPoint.HasDiagram)
             {
-                ImageButton btn = ((ImageButton)currPointLayout.Children.Last());
+                ImageButton pointDiagramBtn = ((ImageButton)currPointGrandparent.Children.Last());
                 bool visibility = true;
 
-                if (btn.IsVisible)
+                if (pointDiagramBtn.IsVisible)
                 {
                     // Rotate expandable icon
-                    currentPointContentLayout.Children.Last().RotateTo(0, 500, Easing.CubicInOut);
+                    currPointParentLayout.Children.Last().RotateTo(0, 500, Easing.CubicInOut);
                     visibility = false;
                 }
                 else
                 {
                     // Rotate expandable icon
-                    currentPointContentLayout.Children.Last().RotateTo(-180, 500, Easing.CubicInOut);
+                    currPointParentLayout.Children.Last().RotateTo(-180, 500, Easing.CubicInOut);
                 }
-                btn.IsVisible = visibility;
+                pointDiagramBtn.IsVisible = visibility;
             }
         }
 
